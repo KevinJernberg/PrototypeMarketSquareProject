717c75c7c6b90450b7f5a95e51de83244517187e
KevinJernberg <a22kevje@student.his.se>
2023-01-14 23:43:11 +0100

    Jump and Some improvement to readability
    Du kan nu hoppa med karaktären, möjligtvis lite buggigt.
    Mer läsbart i player movement test skriptet nu.

--- MarketSquare/Assets/Scripts/TestMovement.cs
+++ MarketSquare/Assets/Scripts/TestMovement.cs
@@ -4,20 +4,50 @@ using UnityEngine;
 
 public class TestMovement : MonoBehaviour
 {
+    //Movement
     [SerializeField] 
     private float moveSpeed;
-    
     [SerializeField] 
     private float rotateSpeed;
+    
+    //Jumping
+    [SerializeField] 
+    private float jumpForce;
+    private float jumpTimer;
+
+    
+    private bool grounded;
+
+    private Rigidbody _rigidbody;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
+    
     // Update is called once per frame
     void Update()
+    {
+        TimersCountdown();
+        
+        GroundedCheck();
+        
+        Movement();
+        Jump();
+    }
+
+    private void TimersCountdown()
+    {
+        if (jumpTimer > 0f)
+        {
+            jumpTimer -= Time.deltaTime;
+        }
+    }
+
+    private void Movement()
     {
         if (Input.GetAxis("Vertical") != 0)
         {
@@ -27,6 +57,28 @@ public class TestMovement : MonoBehaviour
         if (Input.GetAxis("Horizontal") != 0)
         {
             transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+        }
+    }
+
+    private void GroundedCheck()
+    {
+        //Raycast to se if anything is directly below you
+        if (Physics.Raycast(transform.position, Vector3.down, 1.001f))
+        {
+            grounded = true;
+        }
+        else //Nothing directly below you
+        {
+            grounded = false;
+        }
+    }
+
+    private void Jump()
+    {
+        if (jumpTimer <= 0 && Input.GetButton("Jump") && grounded)
+        {
+            jumpTimer = 0.1f;
+            _rigidbody.AddForce(Vector3.up * jumpForce);
         }
     }
 }
